5b2d9f9484cd7c1e5539f293ba807075fe20ab11
keliu@datamesh.com <keliu@datamesh.com>
2021-02-26 15:56:11 +0800

    Create a new project.

---
+++ Assets/Editor/BuildPlayerQuickSetting.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class BuildPlayerQuickSetting : MonoBehaviour
+{
+    private const string scenePath = "Assets/Scenes/";
+
+    [MenuItem("Build/Build To iOS")]
+    public static void BuildToiOS()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { $"{scenePath}SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "iOSBuild";
+        buildPlayerOptions.target = BuildTarget.iOS;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if(summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + "bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+    [MenuItem("Build/Build To Android")]
+    public static void BuildToAndroid()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { $"{scenePath}SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "AndroidBuild";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + "bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+    [MenuItem("Build/Build UWP")]
+    public static void BuildToUWP()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { $"{scenePath}SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "UWPBuild";
+        buildPlayerOptions.target = BuildTarget.WSAPlayer;
+        buildPlayerOptions.options = BuildOptions.None;
+        //buildPlayerOptions options = BuildOptions.
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + "bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+    [MenuItem("Build/Build PC")]
+    public static void BuildToPC()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { $"{scenePath}SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "PCBuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + "bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
